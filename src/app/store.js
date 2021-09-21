542c36268391336afb41edd95eaf18f2fd669f49
Floyden <floydenmonteiro@gmail.com>
2021-09-21 07:59:40 +0530

    ADD: redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import CarReducer from "../features/car/carSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    car: CarReducer,
   },
 });
